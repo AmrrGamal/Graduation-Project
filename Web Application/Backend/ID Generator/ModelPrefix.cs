9071ac62567460c758c15dbc7c5dd436bf658bb9
Amr Gamal <113727947+AmrrGamal@users.noreply.github.com>
2025-07-03 17:50:21 +0300

    Add files via upload

---
+++ Web Application/Backend/ID Generator/ModelPrefix.cs
@@ -0,0 +1,19 @@
+﻿namespace AIDentify.ID_Generator
+{
+    public enum ModelPrefix
+    {
+        Payment = 1,
+        Subscription = 2,
+        Doctor = 3,
+        Student = 4,
+        Patient = 5,
+        Plan = 6,
+        Question = 7,
+        Quiz = 8,
+        QuizAttempt = 9,
+        SystemUpdate = 10,
+        Notification = 11,
+        XRayScan = 12,
+        MedicalHistory = 13
+    }
+}
